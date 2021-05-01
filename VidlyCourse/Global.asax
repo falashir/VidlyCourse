c6921e5212eb2470045eba4c930ef310b63c2829
falashir <falashir@gmail.com>
2021-05-01 13:29:52 +0300

    Add project files.

---
+++ VidlyCourse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyCourse.MvcApplication" Language="C#" %>
---
+++ VidlyCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyCourse.Startup))]
+namespace VidlyCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
