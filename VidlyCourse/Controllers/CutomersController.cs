07e4b8f59af9dfd4c7389132f87ab2a1fecb876b
falashir <falashir@gmail.com>
2021-05-04 15:55:44 +0300

    Second Excercise

--- VidlyCourse/Controllers/CutomersController.cs
+++ VidlyCourse/Controllers/CutomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,10 +11,21 @@ namespace VidlyCourse.Controllers
     public class CustomersController : Controller
     {
 
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Cutomers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             /*                
                new List<Customer>();
@@ -24,22 +36,12 @@ namespace VidlyCourse.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault<Customer>(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault<Customer>(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers() { 
-        
-            return new List<Customer>
-            {
-                new Customer { Id=1, Name = "Ali" },
-                new Customer { Id=2, Name = "Faisal" },
-                new Customer { Id=3, Name = "Mesh" }
-            };
         }
     }
 }
