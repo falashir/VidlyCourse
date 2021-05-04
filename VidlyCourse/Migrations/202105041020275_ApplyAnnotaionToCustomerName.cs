860d8358fee33c3ccffe080b2b2a32d48894792b
falashir <falashir@gmail.com>
2021-05-04 15:56:13 +0300

    Second Excercise

---
+++ VidlyCourse/Migrations/202105041020275_ApplyAnnotaionToCustomerName.cs
@@ -0,0 +1,18 @@
+namespace VidlyCourse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotaionToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
---
+++ VidlyCourse/Migrations/202105041240217_AddNameToMembershipType.cs
@@ -0,0 +1,18 @@
+namespace VidlyCourse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
