865bfdddef50847507ff1a527a8d9f4ac584b7fb
falashir <falashir@gmail.com>
2021-05-08 01:03:08 +0300

    Customer validation

--- VidlyCourse/Controllers/CutomersController.cs
+++ VidlyCourse/Controllers/CutomersController.cs
@@ -62,8 +62,20 @@ namespace VidlyCourse.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
